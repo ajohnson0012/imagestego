116caa7de46cb5eceaa0ddcdacece16b4553bead
Alex Johnson <ajohnson0012@gmail.com>
2016-12-04 11:08:50 -0500

    first commit

---
+++ CMakeFiles/DecryptImage.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DecryptImage.dir/DecryptImage.cpp.o"
+  "DecryptImage.pdb"
+  "DecryptImage"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DecryptImage.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CMakeFiles/EncryptImage.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/EncryptImage.dir/EncryptImage.cpp.o"
+  "EncryptImage.pdb"
+  "EncryptImage"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/EncryptImage.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
